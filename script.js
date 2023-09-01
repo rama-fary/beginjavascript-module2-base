d6bcae735032cb5799e9ee19a3cbb14f7c95ac46
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2023-09-01 21:06:08 +0700

    fix text 3

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-// 🦁 Import cette function
-// import { prompt } from "./helper.js";
+// 🦁 Importe cette fonction
+import { prompt } from "./helper.js";
 
 console.log("NODE-MAILER ™️");
 
 // 🦁 Récupère le mail, l'objet et le message
+let mail = await prompt("Entrez le mail du destinataire : ");
+let objet = await prompt("Entrez l'objet de votre mail : ");
+let message = await prompt("Entrez le message : ");
 
-// 🦁 Affiche une log
-// - Comme quoi le mail est entrain d'être envoyé
+if (mail.length === 0 || objet.length === 0 || message.length === 0) {
+  console.log("Erreur remplir tout les champs !");
+} else {
+  console.log(`The mail is sending... 
+    To : < ${mail}, >
+    Object : < ${objet}, >
+    < ${message}, >`);
+}
+
+// 🦁 Affiche un log
+// - Indiquant que le mail est en train d'être envoyé
 // - Le mail du destinataire
 // - L'objet du mail
 // - Le message du mail
+/*
+console.log("The mail is sending... ");
+console.log("To : <" + mail, ">");
+console.log("Object : <" + objet, ">");
+console.log("<" + message, ">");
+*/
+/*
+console.log(`The mail is sending... 
+To : < ${mail}, >
+Object : < ${objet}, >
+< ${message}, >`);
+*/
